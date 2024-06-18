4d0be2e25dc9094772996ba34a44659d9deb8ec5
CodeAkio <siqueira_2004@hotmail.com>
2024-06-17 22:40:49 -0300

    Add CORS

--- main.py
+++ main.py
@@ -1,19 +1,41 @@
-from fastapi import FastAPI
-from database.connection import conn
-from routes.users import user_router
-from routes.events import event_router
 import uvicorn
+from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+from fastapi.middleware.cors import CORSMiddleware
+
+from database.connection import Settings
+from routes.events import event_router
+from routes.users import user_router
 
 app = FastAPI()
 
-app.include_router(user_router, prefix='/users')
-app.include_router(event_router, prefix='/events')
+settings = Settings()
+
+# register origins
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Register routes
+app.include_router(user_router, prefix="/user")
+app.include_router(event_router, prefix="/event")
 
 
 @app.on_event("startup")
-def on_startup():
-    conn()
+async def init_db():
+    await settings.initialize_database()
+
+
+@app.get("/")
+async def home():
+    return RedirectResponse(url="/event/")
 
 
 if __name__ == '__main__':
-    uvicorn.run("main:app", host='0.0.0.0', port=8080, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
